025222db3f952678381a194146e8240f2cf6595f
1winhyun <jimking1@naver.com>
2025-09-21 18:36:30 +0900

    feat: 게이트웨이 라우트 구현

---
+++ src/main/java/com/unionmate/gateway_service/global/gateway/GatewayConfiguration.java
@@ -0,0 +1,25 @@
+package com.unionmate.gateway_service.global.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+@Profile({"local","dev"})
+public class GatewayConfiguration {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			// 인증 필요 없는 라우트
+			.route("backend_route", r -> r.path("/**")
+				.filters(f -> f
+					.removeRequestHeader(HttpHeaders.COOKIE)
+				)
+				.uri("lb://backend"))
+			.build();
+	}
+}
