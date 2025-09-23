d5ee78532689a9980f910134a3ae7f183d14bf41
1winhyun <jimking1@naver.com>
2025-09-24 01:38:14 +0900

    refactor: 게이트웨이 라우트 설정 prod에 적용되도록 수정

--- src/main/java/com/unionmate/gateway_service/global/gateway/GatewayConfiguration.java
+++ src/main/java/com/unionmate/gateway_service/global/gateway/GatewayConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 
 @Configuration
-@Profile({"local","dev"})
+@Profile({"local","dev","prod"})
 public class GatewayConfiguration {
 
 	@Bean
