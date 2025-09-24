f1fadb2e5ae664a152f13d5893d5705c305c5dec
1winhyun <jimking1@naver.com>
2025-09-24 20:20:21 +0900

    refactor: 스웨거를 위한 라우트 설정

--- src/main/java/com/unionmate/gateway_service/global/gateway/GatewayConfiguration.java
+++ src/main/java/com/unionmate/gateway_service/global/gateway/GatewayConfiguration.java
@@ -20,6 +20,13 @@ public class GatewayConfiguration {
 					.removeRequestHeader(HttpHeaders.COOKIE)
 				)
 				.uri("lb://backend-service"))
+
+			//스웨거를 위한 라우트 설정
+			.route("backend-service_api_docs", r -> r.path("/api-docs/backend/**")
+				.filters(f -> f
+					.rewritePath("/api-docs/bakcend/(?<rem>.*)", "/${rem}")
+				)
+				.uri("lb://backend-service"))
 			.build();
 	}
 }
